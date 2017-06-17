1963cb061bcef1dd83b847f25a97ee53a5bbaa75
Cong <kongvc@gmail.com>
2017-06-17 22:57:52 +0900

    Fix "ifconfig: not found"
    https://github.com/googledatalab/datalab/issues/1438

--- custom-packages.sh
+++ custom-packages.sh
@@ -1,24 +1,24 @@
-apt-get update -y && \
-    apt-get install --no-install-recommends -y -q --allow-unauthenticated \
-        build-essential python3-dev python3-pip git \
-        mecab libmecab-dev mecab-ipadic mecab-ipadic-utf8 &&\
+apt-get update -y && apt-get install --no-install-recommends -y -q \
+  build-essential python3-dev python3-pip git \
+  mecab libmecab-dev mecab-ipadic mecab-ipadic-utf8 \
+  net-tools &&\
 
 ## MeCab
-    # pip  install mecab-python &&\
-    pip3 install mecab-python3 &&\
+  # pip  install mecab-python &&\
+  pip3 install mecab-python3 &&\
 
 ## gensim
-    # pip  install Cython boto google-compute-engine gensim pattern &&\
-    pip3 install Cython boto google-compute-engine gensim &&\
+  # pip  install Cython boto google-compute-engine gensim pattern &&\
+  pip3 install Cython boto google-compute-engine gensim &&\
 
 ## spaCy
-    # pip  install spacy &&\
-    pip3 install spacy &&\
+  # pip  install spacy &&\
+  pip3 install spacy &&\
 
 ## Clean up
-    apt-get purge -y build-essential bzip2 cpp pkg-config libfreetype6-dev &&\
-    apt-get autoremove -y &&\
-    rm -rf /var/lib/apt/lists/* &&\
-    rm -rf /var/lib/dpkg/info/* &&\
-    rm -rf /tmp/* &&\
-    rm -rf /root/.cache/*
+  apt-get purge -y build-essential bzip2 cpp pkg-config libfreetype6-dev &&\
+  apt-get autoremove -y &&\
+  rm -rf /var/lib/apt/lists/* &&\
+  rm -rf /var/lib/dpkg/info/* &&\
+  rm -rf /tmp/* &&\
+  rm -rf /root/.cache/*
