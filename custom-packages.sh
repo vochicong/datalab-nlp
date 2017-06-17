b3538cc85292d3b336d6d03e3b8ceefadc901252
Cong <kongvc@gmail.com>
2017-06-18 00:45:15 +0900

    Add nkf

--- custom-packages.sh
+++ custom-packages.sh
@@ -1,6 +1,6 @@
 apt-get update -y && apt-get install --no-install-recommends -y -q \
   build-essential python3-dev python3-pip git \
-  mecab libmecab-dev mecab-ipadic mecab-ipadic-utf8 \
+  mecab libmecab-dev mecab-ipadic mecab-ipadic-utf8 nkf \
   net-tools &&\
 
 ## MeCab
