7ae1f9111673653d5e6246cb5d94efb218b23a68
Cong <kongvc@gmail.com>
2017-06-20 09:58:41 +0900

    Separate installation and cleanning scripts.
    When you want to install somethings more into a running container,
    just add it to the installation script and run it inside the container.
    At the end of the day, commit it and the docker image will be rebuilt.

--- custom-packages.sh
+++ custom-packages.sh
@@ -1,24 +1,14 @@
+#!/usr/bin/env bash
 apt-get update -y && apt-get install --no-install-recommends -y -q \
-  build-essential python3-dev python3-pip git \
-  mecab libmecab-dev mecab-ipadic mecab-ipadic-utf8 nkf \
-  net-tools &&\
+build-essential python3-dev python3-pip git \
+mecab libmecab-dev mecab-ipadic mecab-ipadic-utf8 nkf \
+net-tools &&\
 
 ## MeCab
-  # pip  install mecab-python &&\
-  pip3 install mecab-python3 &&\
+pip3 install mecab-python3 &&\
 
 ## gensim
-  # pip  install Cython boto google-compute-engine gensim pattern &&\
-  pip3 install Cython boto google-compute-engine gensim &&\
+pip3 install Cython boto google-compute-engine gensim &&\
 
 ## spaCy
-  # pip  install spacy &&\
-  pip3 install spacy &&\
-
-## Clean up
-  apt-get purge -y build-essential bzip2 cpp pkg-config libfreetype6-dev &&\
-  apt-get autoremove -y &&\
-  rm -rf /var/lib/apt/lists/* &&\
-  rm -rf /var/lib/dpkg/info/* &&\
-  rm -rf /tmp/* &&\
-  rm -rf /root/.cache/*
+pip3 install spacy
