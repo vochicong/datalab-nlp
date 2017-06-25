7ab24d281768ba3bb73a312d278be2160a826922
Cong <kongvc@gmail.com>
2017-06-25 17:39:43 +0900

    Add GCloud NL API

--- custom-packages.sh
+++ custom-packages.sh
@@ -4,11 +4,4 @@ build-essential python3-dev python3-pip git \
 mecab libmecab-dev mecab-ipadic mecab-ipadic-utf8 nkf \
 net-tools &&\
 
-## MeCab
-pip3 install mecab-python3 &&\
-
-## gensim
-pip3 install Cython boto google-compute-engine gensim &&\
-
-## spaCy
-pip3 install spacy
+pip3 install -U -r custom-packages-requirements.txt
